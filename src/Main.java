385d0bcfa89d99a85f4e5350cd0d7f6c123cdbc6
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 01:39:39 +0100

    print five to one star by nested loop.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
     int i,j;
     for(i=1; i<=5; i++){
-        for(j=1; j<=i; j++ ) {
+        for(j=5; j>=i; j-- ) {
             System.out.print("*");
         }
         System.out.println();
